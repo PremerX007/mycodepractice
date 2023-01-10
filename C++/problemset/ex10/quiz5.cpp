cec098ee9ca015b871f82a80bbcb7ba69d826fc6
PremerX007 <39229888+PremerX007@users.noreply.github.com>
2023-01-10 14:07:24 +0700

    <3

---
+++ C++/problemset/ex10/quiz5.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int rander(int i,int N){
+    if(i<=N){
+        if(rand()%100 >= 49) return i;
+        else return rander(i+1,N);
+    }else{
+        return i-1;
+    }
+}
+
+int main(){
+    int N;
+    cin >> N;
+    int data[N] = {};
+    srand(time(0));
+    
+    for(int i=1; i<=1000000000; i++){
+        int temp = rander(1,N);
+        data[temp-1]++;
+    }
+
+    for(int i=0; i<N; i++){
+        cout << i+1 << " = " << data[i] << '\n';
+    }
+
+
+}
